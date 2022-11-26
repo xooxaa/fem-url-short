01b79a12d84c011d2973a03d498210d665dd1601
xooxaa <37028661+xooxaa@users.noreply.github.com>
2022-11-26 20:56:29 +0100

    Layout finalized

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const button = document.querySelector(".burger-button");
 const navULs = document.querySelectorAll("nav ul");
 
+button.addEventListener("click", () => {
+  const currentState = button.getAttribute("data-state");
 
+  if (!currentState || currentState === "closed") {
+    button.setAttribute("data-state", "opened");
+    button.setAttribute("aria-expanded", "true");
 
-  button.addEventListener("click", () => {
-    const currentState = button.getAttribute("data-state");
+    navULs.forEach((navUL) => {
+      navUL.classList.remove("hide");
+    });
+  } else {
+    button.setAttribute("data-state", "closed");
+    button.setAttribute("aria-expanded", "false");
 
-    if (!currentState || currentState === "closed") {
-      button.setAttribute("data-state", "opened");
-      button.setAttribute("aria-expanded", "true");
-
-      navULs.forEach((navUL) => {
-        navUL.classList.remove("hide");
-      });
-
-    } else {
-      button.setAttribute("data-state", "closed");
-      button.setAttribute("aria-expanded", "false");
-
-      navULs.forEach((navUL) => {
-        navUL.classList.add("hide");
-      });
-    }
-  });
+    navULs.forEach((navUL) => {
+      navUL.classList.add("hide");
+    });
+  }
+});
