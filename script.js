aa2809fc12c4335e257c937eb969e992cb758079
xooxaa <37028661+xooxaa@users.noreply.github.com>
2022-11-28 18:46:41 +0100

    Done

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const button = document.querySelector(".burger-button");
 const navULs = document.querySelectorAll("nav ul");
+const urlShortener = document.querySelector(".url-shortener");
+const urlShortenerInput = document.querySelector("#url-input");
+const urlShortenerButton = document.querySelector("#url-button");
+const urlShortenerInfo = document.querySelector("#url-info");
 
+//***********************************************************************************************
+//*************************************************************************** Burger Menu Toggle
 button.addEventListener("click", () => {
   const currentState = button.getAttribute("data-state");
 
@@ -21,64 +27,160 @@ button.addEventListener("click", () => {
   }
 });
 
-const urlShortener = document.querySelector(".url-shortener");
-const urlShortenerInput = document.querySelector("#url-input");
-const urlShortenerButton = document.querySelector("#url-button");
-const urlShortenerInfo = document.querySelector("#url-info");
-const urlShortenerInfoButton = document.querySelector("#url-info-button");
+//***********************************************************************************************
+//******************************************************************************** Local Storage
+function updateLocalStorage() {
+  let links = [];
 
-urlShortenerInfo.innerText =
-  "Enter a link you would like to shorten above. Its just that easy!";
+  for (let link of urlShortener.children) {
+    console.log(link.children[0].classList[0]);
+    if (link.children[0].classList[0] === "long-link") {
+      links.push({
+        long_link: link.children[0].innerText,
+        full_short_link: link.children[1].href,
+        short_link: link.children[1].innerText,
+      });
+    }
+  }
+  localStorage.setItem("links", JSON.stringify(links));
+}
 
-urlShortenerInfoButton.addEventListener("click", () => {
-  urlShortenerInput.focus();
-});
+function readFromLocalStorage() {
+  let links = [];
+
+  links = JSON.parse(localStorage.getItem("links"));
+  if (links) {
+    for (let link of links) {
+      const newshortURL = document.createElement("div");
+      newshortURL.classList.add("short-url");
+
+      const newLongLinkSpan = document.createElement("span");
+      newLongLinkSpan.innerText = link.long_link;
+      newLongLinkSpan.classList.add("long-link");
+
+      const newShortLinkSpan = document.createElement("span");
+      newShortLinkSpan.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="${link.full_short_link}">${link.short_link}</a>`;
+      newShortLinkSpan.classList.add("short-link");
+
+      const newCopyButton = document.createElement("button");
+      newCopyButton.innerText = "Copy";
+      newCopyButton.setAttribute("type", "button");
+
+      const newRemoveButton = document.createElement("button");
+      newRemoveButton.innerText = "Remove";
+      newRemoveButton.setAttribute("type", "button");
+      newRemoveButton.classList.add("remove");
+
+      newshortURL.append(newLongLinkSpan);
+      newshortURL.append(newShortLinkSpan);
+      newshortURL.append(newCopyButton);
+      newshortURL.append(newRemoveButton);
+
+      urlShortener.append(newshortURL);
+    }
+  }
+}
+
+readFromLocalStorage();
+
+//***********************************************************************************************
+//******************************************************************************* URL Shortening
+
+urlShortenerInfo.innerText = "";
 
 const shortenURL = async () => {
+  urlShortenerInfo.innerText = "";
   if (!urlShortenerInput.value) {
-    urlShortenerInfo.innerText = "Please enter a link above!";
+    urlShortenerInfo.innerText = "Please add a link!";
+    urlShortenerInput.style.outline = "2px solid hsl(0, 87%, 67%)";
   } else {
     const shortenURLquery =
       "https://api.shrtco.de/v2/shorten?url=" + urlShortenerInput.value;
+    urlShortenerInput.value = "";
+
     await fetch(shortenURLquery, { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        console.log(data.result.short_link);
+        if (data.ok) {
+          console.log(data.result.short_link);
 
-        //create new short URL container
-        const newshortURL = document.createElement("div");
-        newshortURL.classList.add("short-url");
+          const newshortURL = document.createElement("div");
+          newshortURL.classList.add("short-url");
 
-        //create new todo span
-        const newLongLinkSpan = document.createElement("span");
-        newLongLinkSpan.innerText = data.result.original_link;
-        newLongLinkSpan.classList.add("long-link");
+          const newLongLinkSpan = document.createElement("span");
+          newLongLinkSpan.innerText = data.result.original_link;
+          newLongLinkSpan.classList.add("long-link");
 
-        //create new todo span
-        const newShortLinkSpan = document.createElement("span");
-        newShortLinkSpan.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="${data.result.full_short_link}">${data.result.short_link}</a>`;
-        newShortLinkSpan.classList.add("short-link");
+          const newShortLinkSpan = document.createElement("span");
+          newShortLinkSpan.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="${data.result.full_short_link}">${data.result.short_link}</a>`;
+          newShortLinkSpan.classList.add("short-link");
 
-        //create new cross img
-        const newCopyButton = document.createElement("button");
-        newCopyButton.innerText = "Copy";
-        newCopyButton.setAttribute("type", "button");
-        newCopyButton.setAttribute("name", "button");
+          const newCopyButton = document.createElement("button");
+          newCopyButton.innerText = "Copy";
+          newCopyButton.setAttribute("type", "button");
 
-        //append checkmark, span and cross to todo container
-        newshortURL.append(newLongLinkSpan);
-        newshortURL.append(newShortLinkSpan);
-        newshortURL.append(newCopyButton);
+          const newRemoveButton = document.createElement("button");
+          newRemoveButton.innerText = "Remove";
+          newRemoveButton.setAttribute("type", "button");
+          newRemoveButton.classList.add("remove");
 
-        //append todo container to todosList
-        urlShortener.append(newshortURL);
+          newshortURL.append(newLongLinkSpan);
+          newshortURL.append(newShortLinkSpan);
+          newshortURL.append(newCopyButton);
+          newshortURL.append(newRemoveButton);
+
+          urlShortener.append(newshortURL);
+
+          updateLocalStorage();
+        } else {
+          urlShortenerInput.style.outline = "2px solid hsl(0, 87%, 67%)";
+          if (data.error_code === 2) {
+            urlShortenerInfo.innerText =
+              "Invalid URL submitted: Pleas check your input.";
+          }
+          if (data.error_code === 10) {
+            urlShortenerInfo.innerText =
+              "The url you entered is not allowed: Please enter a different URL";
+          }
+        }
       });
-
-    // urlShortenerInfo.innerText = res.result.short_link;
-
-    //https://api.shrtco.de/v2/shorten?url=example.org/very/long/link.html
   }
 };
 
 urlShortenerButton.addEventListener("click", shortenURL);
+
+urlShortenerInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    shortenURL();
+  }
+});
+
+urlShortenerInput.addEventListener("input", () => {
+  urlShortenerInfo.innerText = "";
+  urlShortenerInput.style.outline = "none";
+});
+
+urlShortener.addEventListener("click", (e) => {
+  console.log(e.target.innerText);
+  if (e.target.innerText == "Copy") {
+    for (let child of urlShortener.children) {
+      if (child.children[2].innerText == "Copied!") {
+        child.children[2].classList.remove("copied");
+        child.children[2].innerText = "Copy";
+      }
+    }
+
+    navigator.clipboard.writeText(e.target.previousSibling.innerText);
+    e.target.classList.add("copied");
+    e.target.innerText = "Copied!";
+    setTimeout(function () {
+      e.target.classList.remove("copied");
+      e.target.innerText = "Copy";
+    }, 2000);
+  }
+  if (e.target.innerText == "Remove") {
+    e.target.parentElement.remove();
+    updateLocalStorage();
+  }
+});
